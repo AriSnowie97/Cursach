2b15e9417e85ba939f3e6cffa7bdbe059ce4fe45
AriSnowie97 <diysylveon@gmail.com>
2026-03-30 17:04:15 +0300

    updated functions

--- Frontend/script.js
+++ Frontend/script.js
@@ -25,5 +25,35 @@ async function loadOrders() {
     }
 }
 
+async function createOrder(event) {
+    event.preventDefault(); // Щоб сторінка не моргала при натисканні
+
+    const newOrder = {
+        title: document.getElementById('title').value,
+        description: document.getElementById('desc').value,
+        budget: parseFloat(document.getElementById('budget').value),
+        deadline: new Date(document.getElementById('deadline').value).toISOString(),
+        status: "Open",
+        customerId: 1 
+    };
+
+    try {
+        const response = await fetch('http://localhost:5245/api/orders', {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify(newOrder)
+        });
+
+        if (response.ok) {
+            document.getElementById('orderForm').reset(); // Очищаємо поля
+            loadOrders(); // Одразу показуємо нове замовлення
+        } else {
+            alert('Помилка сервера. Замовлення не створено.');
+        }
+    } catch (error) {
+        alert('Немає зв\'язку з бекендом!');
+    }
+}
+
 // Завантажуємо дані відразу при відкритті сторінки
 loadOrders();
