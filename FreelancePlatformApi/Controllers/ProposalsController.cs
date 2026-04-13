548472551b293e78109a787a4d0c4aac077ecc00
AriSnowie97 <diysylveon@gmail.com>
2026-04-13 14:24:11 +0300

    Додано метод для отримання пропозицій користувача за його ім'ям у контролері ProposalsController; створено нову сторінку MyProposals для відображення пропозицій фрілансера.

--- FreelancePlatformApi/Controllers/ProposalsController.cs
+++ FreelancePlatformApi/Controllers/ProposalsController.cs
@@ -1,7 +1,10 @@
-using FreelancePlatformApi.Data;
-using FreelancePlatformApi.Models;
 using Microsoft.AspNetCore.Mvc;
+using FreelancePlatformApi.Data;
 using Microsoft.EntityFrameworkCore;
+using FreelancePlatformApi.Models; // Зверни увагу, щоб простір імен збігався з твоїм
+using System.Linq;
+using System.Threading.Tasks;
+using System.Collections.Generic;
 
 namespace FreelancePlatformApi.Controllers
 {
@@ -16,20 +19,34 @@ namespace FreelancePlatformApi.Controllers
             _context = context;
         }
 
-        // GET: api/proposals
-        [HttpGet]
-        public async Task<ActionResult<IEnumerable<Proposal>>> GetProposals()
+        // GET: api/proposals/user/{name}
+        [HttpGet("user/{name}")]
+        public async Task<ActionResult<IEnumerable<Proposal>>> GetUserProposals(string name)
         {
-            return await _context.Proposals.ToListAsync();
+            // 1. Спочатку знаходимо фрілансера за його ім'ям (яке прийшло з фронтенду)
+            var user = await _context.Users.FirstOrDefaultAsync(u => u.Name == name);
+            
+            if (user == null) 
+            {
+                return NotFound("Користувача не знайдено");
+            }
+
+            // 2. Дістаємо всі пропозиції цього фрілансера
+            // УВАГА: Якщо в твоїй моделі Proposal поле називається FreelancerId замість UserId,
+            // просто заміни p.UserId на p.FreelancerId у рядку нижче!
+            var proposals = await _context.Proposals
+                .Where(p => p.FreelancerId == user.Id) 
+                .ToListAsync();
+
+            return Ok(proposals);
         }
 
-        // POST: api/proposals
+        // POST: api/proposals (одразу додаємо метод для створення нових відгуків на майбутнє!)
         [HttpPost]
         public async Task<ActionResult<Proposal>> CreateProposal(Proposal proposal)
         {
             _context.Proposals.Add(proposal);
             await _context.SaveChangesAsync();
-
             return Ok(proposal);
         }
     }
